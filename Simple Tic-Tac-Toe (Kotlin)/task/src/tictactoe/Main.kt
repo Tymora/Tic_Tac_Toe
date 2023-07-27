0bf434f406b156b624bd4d26e19ef0bfb4e865ab
Tymora <dachabel@mail.ru>
2023-07-27 06:02:59 +0300

    Фикс ошибки и чистка неиспользуемого

--- Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
@@ -23,8 +23,7 @@ fun main() {
                 countCI++
             }
         }
-
-    } while (!inputList.contains(' ') || !inputList.contains('_'))
+    } while (inputList.contains(' '))
 
     when (stat(inputList)) {
         in 1..2 -> println("X wins")
@@ -69,22 +68,24 @@ fun stat(inputList: MutableList<Char>): Int {
     } else if (inputList.slice(setOf(6, 4, 2)).all { it == circle }) {
         status += 10
     }
+
     if (inputList.slice(setOf(0, 3, 6)).all { it == cross }) {
         status++
     } else if (inputList.slice(setOf(0, 3, 6)).all { it == circle }) {
         status += 10
     }
+
     if (inputList.slice(setOf(1, 4, 7)).all { it == cross }) {
         status++
     } else if (inputList.slice(setOf(1, 4, 7)).all { it == circle }) {
         status += 10
     }
+
     if (inputList.slice(setOf(2, 5, 8)).all { it == cross }) {
         status++
     } else if (inputList.slice(setOf(2, 5, 8)).all { it == circle }) {
         status += 10
     }
-
 
     if (status == 0 && (inputList.contains(' ')) || (inputList.contains('_'))) {
         status += 100
@@ -100,95 +101,82 @@ fun crossRound(inputList: MutableList<Char>) {
         val (coord1, coord2) = readln().split(" ")
         if (coord1.toIntOrNull() == null && coord2.toIntOrNull() == null) {
             println("You should enter numbers!")
-            continue
+
         } else if (coord1.toInt() in 1..3 && coord2.toInt() in 1..3) {
             when (coord1.toInt()) {
                 1 -> when (coord2.toInt()) {
-                    1 -> if (inputList[0] == ' ' || inputList[0] == '_') {
+                    1 -> if (inputList[0] == ' ') {
                         inputList[0] = 'X'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    2 -> if (inputList[1] == ' ' || inputList[1] == '_') {
+                    2 -> if (inputList[1] == ' ') {
                         inputList[1] = 'X'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    3 -> if (inputList[2] == ' ' || inputList[2] == '_') {
+                    3 -> if (inputList[2] == ' ') {
                         inputList[2] = 'X'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-
                 }
                 2 -> when (coord2.toInt()) {
-                    1 -> if (inputList[3] == ' ' || inputList[3] == '_') {
+                    1 -> if (inputList[3] == ' ') {
                         inputList[3] = 'X'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    2 -> if (inputList[4] == ' ' || inputList[4] == '_') {
+                    2 -> if (inputList[4] == ' ') {
                         inputList[4] = 'X'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    3 -> if (inputList[5] == ' ' || inputList[5] == '_') {
+                    3 -> if (inputList[5] == ' ') {
                         inputList[5] = 'X'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-
                 }
                 3 -> when (coord2.toInt()) {
-                    1 -> if (inputList[6] == ' ' || inputList[6] == '_') {
+                    1 -> if (inputList[6] == ' ') {
                         inputList[6] = 'X'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    2 -> if (inputList[7] == ' ' || inputList[7] == '_') {
+                    2 -> if (inputList[7] == ' ') {
                         inputList[7] = 'X'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    3 -> if (inputList[8] == ' ' || inputList[8] == '_') {
+                    3 -> if (inputList[8] == ' ') {
                         inputList[8] = 'X'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-
                 }
             }
-
         } else {
             println("Coordinates should be from 1 to 3!")
-            continue
+
         }
     }
 }
@@ -198,87 +186,77 @@ fun circleRound(inputList: MutableList<Char>) {
         val (coord1, coord2) = readln().split(" ")
         if (coord1.toIntOrNull() == null && coord2.toIntOrNull() == null) {
             println("You should enter numbers!")
-            continue
+
         } else if (coord1.toInt() in 1..3 && coord2.toInt() in 1..3) {
             when (coord1.toInt()) {
                 1 -> when (coord2.toInt()) {
-                    1 -> if (inputList[0] == ' ' || inputList[0] == '_') {
+                    1 -> if (inputList[0] == ' ') {
                         inputList[0] = 'O'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    2 -> if (inputList[1] == ' ' || inputList[1] == '_') {
+                    2 -> if (inputList[1] == ' ') {
                         inputList[1] = 'O'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    3 -> if (inputList[2] == ' ' || inputList[2] == '_') {
+                    3 -> if (inputList[2] == ' ') {
                         inputList[2] = 'O'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-
                 }
                 2 -> when (coord2.toInt()) {
-                    1 -> if (inputList[3] == ' ' || inputList[3] == '_') {
+                    1 -> if (inputList[3] == ' ') {
                         inputList[3] = 'O'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    2 -> if (inputList[4] == ' ' || inputList[4] == '_') {
+                    2 -> if (inputList[4] == ' ') {
                         inputList[4] = 'O'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    3 -> if (inputList[5] == ' ' || inputList[5] == '_') {
+                    3 -> if (inputList[5] == ' ') {
                         inputList[5] = 'O'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
 
                 }
                 3 -> when (coord2.toInt()) {
-                    1 -> if (inputList[6] == ' ' || inputList[6] == '_') {
+                    1 -> if (inputList[6] == ' ') {
                         inputList[6] = 'O'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    2 -> if (inputList[7] == ' ' || inputList[7] == '_') {
+                    2 -> if (inputList[7] == ' ') {
                         inputList[7] = 'O'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
-                    3 -> if (inputList[8] == ' ' || inputList[8] == '_') {
+                    3 -> if (inputList[8] == ' ') {
                         inputList[8] = 'O'
                         displaySquare(inputList)
                         break
                     } else {
                         println("This cell is occupied! Choose another one!")
-                        continue
                     }
 
                 }
@@ -286,7 +264,7 @@ fun circleRound(inputList: MutableList<Char>) {
 
         } else {
             println("Coordinates should be from 1 to 3!")
-            continue
+
         }
     }
 }
