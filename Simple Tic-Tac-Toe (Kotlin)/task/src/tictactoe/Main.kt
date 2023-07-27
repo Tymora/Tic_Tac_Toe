9052906193c4769bbb1d99a483360b2e8145ae17
Tymora <dachabel@mail.ru>
2023-07-27 05:37:33 +0300

    Доделала игру, но надо еще протестировать и убрать лишнее из кода.

--- Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe (Kotlin)/task/src/tictactoe/Main.kt
@@ -1,82 +1,101 @@
 package tictactoe
 
 fun main() {
-    val inp = readln()
+    var countCR = 0
+    var countCI = 0
     val inputList = mutableListOf<Char>()
-    for (i in inp) {
-        inputList.add(i)
+    for (i in 1..9) {
+        inputList.add(' ')
     }
     displaySquare(inputList)
+    do {
+        when (stat(inputList)) {
+            1, 2, 10, 20 -> break
+        }
+        when {
+            countCR == 0 || countCR == countCI -> {
+                crossRound(inputList)
+                countCR++
+
+            }
+            countCR > countCI -> {
+                circleRound(inputList)
+                countCI++
+            }
+        }
+
+    } while (!inputList.contains(' ') || !inputList.contains('_'))
+
+    when (stat(inputList)) {
+        in 1..2 -> println("X wins")
+        10, 20 -> println("O wins")
+        11 -> println("Impossible")
+//        100 -> println("Game not finished")
+        200 -> println("Draw")
+    }
+}
+
+fun stat(inputList: MutableList<Char>): Int {
     val cross = 'X'
     val circle = 'O'
-    var countcross = 0
-    var countcircle = 0
     var status = 0
-    for (i in inputList) {
-        if (i == cross) {
-            countcross++
-        } else if (i == circle) {
-            countcircle++
-        }
+
+    if (inputList.slice(0..2).all { it == cross }) {
+        status++
+    } else if (inputList.slice(0..2).all { it == circle }) {
+        status += 10
     }
-    if (countcircle == countcross || (countcircle + 1 == countcross) || (countcircle - 1 == countcross)) {
+
+    if (inputList.slice(3..5).all { it == cross }) {
+        status++
+    } else if (inputList.slice(3..5).all { it == circle }) {
+        status += 10
+    }
+
+    if (inputList.slice(6..8).all { it == cross }) {
+        status++
+    } else if (inputList.slice(6..8).all { it == circle }) {
+        status += 10
+    }
+
+    if (inputList.slice(setOf(0, 4, 8)).all { it == cross }) {
+        status++
+    } else if (inputList.slice(setOf(0, 4, 8)).all { it == circle }) {
+        status += 10
+    }
+
+    if (inputList.slice(setOf(6, 4, 2)).all { it == cross }) {
+        status++
+    } else if (inputList.slice(setOf(6, 4, 2)).all { it == circle }) {
+        status += 10
+    }
+    if (inputList.slice(setOf(0, 3, 6)).all { it == cross }) {
+        status++
+    } else if (inputList.slice(setOf(0, 3, 6)).all { it == circle }) {
+        status += 10
+    }
+    if (inputList.slice(setOf(1, 4, 7)).all { it == cross }) {
+        status++
+    } else if (inputList.slice(setOf(1, 4, 7)).all { it == circle }) {
+        status += 10
+    }
+    if (inputList.slice(setOf(2, 5, 8)).all { it == cross }) {
+        status++
+    } else if (inputList.slice(setOf(2, 5, 8)).all { it == circle }) {
+        status += 10
+    }
 
 
-        if (inputList.slice(0..2).all { it == cross }) {
-            status++
-        } else if (inputList.slice(0..2).all { it == circle }) {
-            status += 10
-        }
+    if (status == 0 && (inputList.contains(' ')) || (inputList.contains('_'))) {
+        status += 100
+    } else if (status == 0) {
+        status += 200
+    }
 
-        if (inputList.slice(3..5).all { it == cross }) {
-            status++
-        } else if (inputList.slice(3..5).all { it == circle }) {
-            status += 10
-        }
+    return status
+}
 
-        if (inputList.slice(6..8).all { it == cross }) {
-            status++
-        } else if (inputList.slice(6..8).all { it == circle }) {
-            status += 10
-        }
-
-        if (inputList.slice(setOf(0, 4, 8)).all { it == cross }) {
-            status++
-        } else if (inputList.slice(setOf(0, 4, 8)).all { it == circle }) {
-            status += 10
-        }
-
-        if (inputList.slice(setOf(6, 4, 2)).all { it == cross }) {
-            status++
-        } else if (inputList.slice(setOf(6, 4, 2)).all { it == circle }) {
-            status += 10
-        }
-        if (inputList.slice(setOf(0, 3, 6)).all { it == cross }) {
-            status++
-        } else if (inputList.slice(setOf(0, 3, 6)).all { it == circle }) {
-            status += 10
-        }
-        if (inputList.slice(setOf(1, 4, 7)).all { it == cross }) {
-            status++
-        } else if (inputList.slice(setOf(1, 4, 7)).all { it == circle }) {
-            status += 10
-        }
-        if (inputList.slice(setOf(2, 5, 8)).all { it == cross }) {
-            status++
-        } else if (inputList.slice(setOf(2, 5, 8)).all { it == circle }) {
-            status += 10
-        }
-
-
-        if (status == 0 && (inputList.contains(' ')) || (inputList.contains('_'))) {
-            status += 100
-        } else if (status == 0) {
-            status += 200
-        }
-
-
-    } else println("Impossible")
-
+fun crossRound(inputList: MutableList<Char>) {
     while (true) {
         val (coord1, coord2) = readln().split(" ")
         if (coord1.toIntOrNull() == null && coord2.toIntOrNull() == null) {
@@ -172,14 +191,106 @@ fun main() {
             continue
         }
     }
-//    when(status){
-//        in 1..2 -> println("X wins")
-//        10, 20 -> println("O wins")
-//        11 -> println("Impossible")
-//        100 -> println("Game not finished")
-//        200 -> println("Draw")
-//    }
 }
+
+fun circleRound(inputList: MutableList<Char>) {
+    while (true) {
+        val (coord1, coord2) = readln().split(" ")
+        if (coord1.toIntOrNull() == null && coord2.toIntOrNull() == null) {
+            println("You should enter numbers!")
+            continue
+        } else if (coord1.toInt() in 1..3 && coord2.toInt() in 1..3) {
+            when (coord1.toInt()) {
+                1 -> when (coord2.toInt()) {
+                    1 -> if (inputList[0] == ' ' || inputList[0] == '_') {
+                        inputList[0] = 'O'
+                        displaySquare(inputList)
+                        break
+                    } else {
+                        println("This cell is occupied! Choose another one!")
+                        continue
+                    }
+                    2 -> if (inputList[1] == ' ' || inputList[1] == '_') {
+                        inputList[1] = 'O'
+                        displaySquare(inputList)
+                        break
+                    } else {
+                        println("This cell is occupied! Choose another one!")
+                        continue
+                    }
+                    3 -> if (inputList[2] == ' ' || inputList[2] == '_') {
+                        inputList[2] = 'O'
+                        displaySquare(inputList)
+                        break
+                    } else {
+                        println("This cell is occupied! Choose another one!")
+                        continue
+                    }
+
+                }
+                2 -> when (coord2.toInt()) {
+                    1 -> if (inputList[3] == ' ' || inputList[3] == '_') {
+                        inputList[3] = 'O'
+                        displaySquare(inputList)
+                        break
+                    } else {
+                        println("This cell is occupied! Choose another one!")
+                        continue
+                    }
+                    2 -> if (inputList[4] == ' ' || inputList[4] == '_') {
+                        inputList[4] = 'O'
+                        displaySquare(inputList)
+                        break
+                    } else {
+                        println("This cell is occupied! Choose another one!")
+                        continue
+                    }
+                    3 -> if (inputList[5] == ' ' || inputList[5] == '_') {
+                        inputList[5] = 'O'
+                        displaySquare(inputList)
+                        break
+                    } else {
+                        println("This cell is occupied! Choose another one!")
+                        continue
+                    }
+
+                }
+                3 -> when (coord2.toInt()) {
+                    1 -> if (inputList[6] == ' ' || inputList[6] == '_') {
+                        inputList[6] = 'O'
+                        displaySquare(inputList)
+                        break
+                    } else {
+                        println("This cell is occupied! Choose another one!")
+                        continue
+                    }
+                    2 -> if (inputList[7] == ' ' || inputList[7] == '_') {
+                        inputList[7] = 'O'
+                        displaySquare(inputList)
+                        break
+                    } else {
+                        println("This cell is occupied! Choose another one!")
+                        continue
+                    }
+                    3 -> if (inputList[8] == ' ' || inputList[8] == '_') {
+                        inputList[8] = 'O'
+                        displaySquare(inputList)
+                        break
+                    } else {
+                        println("This cell is occupied! Choose another one!")
+                        continue
+                    }
+
+                }
+            }
+
+        } else {
+            println("Coordinates should be from 1 to 3!")
+            continue
+        }
+    }
+}
+
 
 fun displaySquare(inputList: MutableList<Char>) {
 
